1818d3e59bf5ee86627306b6432f0d5b66b72e88
Anis LARGUET <a.larguet@comunikmail.com>
2023-04-08 03:13:47 +0100

    update prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
-import prismadb from "@/libs/prismadb"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import NextAuth from "next-auth"
 import Credentials from 'next-auth/providers/credentials';
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prismadb from "@/libs/prismadb"
 
 import {compare} from "bcrypt"
 export default NextAuth({
